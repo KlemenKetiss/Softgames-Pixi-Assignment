f98fae2d5a33def4b83aed7e9276eb4bc71cae70
klemenketis <klemen.ketis@gmail.com>
2026-03-12 21:10:38 +0100

    Updated vite config and deploy file to resole MIME issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: '.',
   publicDir: 'public',
+  base: '/Softgames-Pixi-Assignment/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
